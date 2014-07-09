8a2273bd11062637918593a6a158090eeeb7e745
Somnuk Wongkhan <somnuk.wk@gmail.com>
2014-07-09 21:27:40 +0700

    split function

--- src/main.go
+++ src/main.go
@@ -4,9 +4,22 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
+	
+	r.GET("/ping", Ping)
+	r.GET("/", Index)
+	r.GET("/test", Test)
 	
 	r.Run(":8000")
+}
+
+func Ping(c *gin.Context) {
+	c.String(200, "Pong")
+}
+
+func Index(c *gin.Context) {
+	c.String(200, "Index")
+}
+
+func Test(c *gin.Context) {
+	c.String(200, "Test")
 }
